3681e3bf559f15e630e321e88adbea21545b55a2
Melissa Guachun <1212malka@gmail.com>
2021-08-03 14:14:27 -0400

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,12 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-genres = Genre.create([{ name: 'rock' }, { name: 'country' }, { name: 'hip-hop'}, { name: 'pop'}, { name: 'r&b'}, { name: 'punk'}, { name: 'soul'}])
+genres = Genre.create!([{ name: 'rock' }, { name: 'jazz' }, { name: 'metal' }, { name: 'country' }, { name: 'hip-hop'}, { name: 'pop'}, 
+{ name: 'r&b'}, { name: 'punk'}, { name: 'soul'}, { name: 'disco'}, { name: 'folk'}])
+
+albums = Album.create([{ title: 'The Dark Side of the Moon', artist: 'Pink Floyd', year: '1973', genre: 'rock'}, { title: 'Come On Over', artist: 'Shania Twain', year: '1997', genre: 'country'},
+{ title: 'The Immaculate Collection', artist: 'Madonna', year: '1990', genre: 'pop'}, { title: 'Born in the U.S.A', artist: 'Bruce Springsteen', year: '1984', genre: 'rock'},
+{ title: 'Nevermind', artist: 'Nirvana', year: '1991', genre: 'rock'}, { title: '...Baby One More Time', artist: 'Britney Spears', year: '1999', genre: 'pop'}, 
+{ title: 'Bridge over Troubled Water', artist: 'Simon & Garfunkel', year: '1970', genre: 'folk'}, { title: 'Purple Rain', artist: 'Prince and the Revolution', year: '1984', genre: 'pop'},
+{ title: 'The Marshall Mathers LP', artist: 'Eminem', year: '2000', genre: 'hip-hop'}, { title: 'The Miseducation of Lauryn Hill', artist: 'Lauryn Hill', year: '1998', genre: 'soul'},
+{ title: 'Daydream', artist: 'Mariah Carey', year: '1995', genre: 'r&b'}, { title: 'Faith', artist: 'George Michaels', year: '1987', genre: 'pop'} ])
