1c06365ff2c351c9d6480be218a5fd5b0188a784
Melissa Guachun <1212malka@gmail.com>
2021-08-03 14:13:56 -0400

    added to controller

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,11 +1,48 @@
 class AlbumsController < ApplicationController
+    before_action :set_album, except: [:index, :create]
     def index 
+        albums = Album.all
+        render json: albums
     end
 
     def show
+        render json: @albums,
+        except: [:created_at, :updated_at]
+    end
+
+    def create 
+        album = Album.new(album_params)
+        if album.save
+        render json: album,
+        except: [:created_at, :updated_at]
+        else 
+            render json: {message: 'Unable to create album.'}
+        end
+    end
+
+    def update
+        @album.update(album_params)
+        if @album.save
+            render json: @album,
+            except: [:created_at, :updated_at]
+        else 
+            render json: {message: 'Unable to edit album.'}
     end
 
     def destroy
+        @album.destroy
     end
+
+    private
+
+    def set_album
+        @album = Album.find(params[:id])
+    end
+
+    def album_params
+        params.require(:album).permit(:title, :artist: :year, :genre_id)
+    end
+
+
 
 end
