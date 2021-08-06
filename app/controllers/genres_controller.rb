037b8aa8048c7f50b07fe2efc9024f1e96eca364
Melissa Guachun <1212malka@gmail.com>
2021-08-05 20:29:13 -0400

    genre controller spacing edit

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -3,7 +3,6 @@ class GenresController < ApplicationController
         genres = Genre.all 
         render json: genres,
         except: [:created_at, :updated_at]
-
     end
 
     def show
@@ -11,8 +10,44 @@ class GenresController < ApplicationController
         except: [:created_at, :updated_at]
     end
 
+    def create 
+        genre = Genre.new(genre_params)
+        
+        if genre.save
+            render json: genre,
+            only: [:name],
+            include: { albums: {only: [:title, :artist, :year, :genre_id]} }
+        else
+            render json: {error: "Genre not created."}
+        end
+    end 
+
+    def update
+        genre = Genre.find(params[:id])
+        genre.update(genre_params)
+
+        if genre.save
+            render json: genre,
+            only: [:name]
+            include: { albums: {only: [:title, :artist, :year, :genre_id]} }
+        else
+            render json: {error: "Genre not updated."}
+        end
+    end
+
+
     def destroy
-        @genre.destroy
+        genre = Genre.find(params[:id])
+        genre.destroy
+        render json: [message: "#{genre.name} successfully deleted."]
+    end
+    #should I add id to the destroy method?
+
+
+    private 
+    
+    def genre_params
+        params.require(:genre).permit(:name)
     end
 
 end
