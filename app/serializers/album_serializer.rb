3d3ea04239075183491b0fd5e5c824b75e946023
Melissa Guachun <1212malka@gmail.com>
2021-08-13 14:19:29 -0400

    added year

--- app/serializers/album_serializer.rb
+++ app/serializers/album_serializer.rb
@@ -1,4 +1,4 @@
 class AlbumSerializer < ActiveModel::Serializer
-  attributes :title, :artist, :genre, :genre_id, :id
+  attributes :title, :artist, :year, :genre, :genre_id, :id
   belongs_to :genre
 end
